dc5679c8592346dea9a1285ec91e6ddd297f24fa
jonortega <jon.ortega.g@gmail.com>
2022-11-13 18:03:47 +0100

    JSOn formatting solucionado

--- js/stats.js
+++ js/stats.js
@@ -1,21 +1,23 @@
-export {initState}
+export { initState }
 
-let initState = function(what, solutionId) { 
+let initState = function (what, solutionId) {
 
     let solucion = []
     let lista = localStorage.getItem(what)
-    if(lista == null){
-        localStorage.setItem(what, {"guesses":[], "solution":solutionId})
+    console.log(lista)
+    if (lista == null) {
+        localStorage.setItem(what, JSON.stringify({ "guesses": [], "solution": solutionId }))
         lista = localStorage.getItem(what)
     }
     solucion[0] = lista
     solucion[1] = ((guess) => {
+        let lista = JSON.parse(localStorage.getItem('WAYgameState'))
         console.log(lista.guesses)
         lista.guesses.push(guess)
-        localStorage.setItem(what, lista)
+        localStorage.setItem('WAYgameState', JSON.stringify(lista))
     })
 
     console.log(solucion)
 
     return solucion
-}
+}   
