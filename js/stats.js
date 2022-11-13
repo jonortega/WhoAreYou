fec566d83d40f471ddc47b38ba6f742ae013edfe
paabloomaartinez <pablo121002@gmail.com>
2022-11-13 18:03:05 +0100

    solucionado initState

--- js/stats.js
+++ js/stats.js
@@ -4,15 +4,16 @@ let initState = function(what, solutionId) {
 
     let solucion = []
     let lista = localStorage.getItem(what)
-    if(lista == null){
-        localStorage.setItem(what, {"guesses":[], "solution":solutionId})
+    if(lista === null){
+        localStorage.setItem(what, JSON.stringify({"guesses":[], "solution":solutionId}))
         lista = localStorage.getItem(what)
     }
     solucion[0] = lista
     solucion[1] = ((guess) => {
+        let lista = JSON.parse(localStorage.getItem(what))
         console.log(lista.guesses)
         lista.guesses.push(guess)
-        localStorage.setItem(what, lista)
+        localStorage.setItem(what, JSON.stringify(lista))
     })
 
     console.log(solucion)
