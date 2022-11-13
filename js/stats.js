0b266d701f2760b13ffd404e9fe6f850282f61af
jonortega <jon.ortega.g@gmail.com>
2022-11-13 19:18:09 +0100

    updateStats created

--- js/stats.js
+++ js/stats.js
@@ -1,11 +1,11 @@
-export {updateStats, getStats, initState}
+export { updateStats, getStats, initState }
 
 let initState = function (what, solutionId) {
 
     let solucion = []
     let lista = localStorage.getItem(what)
-    if(lista == null){
-        localStorage.setItem(what, JSON.stringify({"guesses":[], "solution":solutionId}))
+    if (lista == null) {
+        localStorage.setItem(what, JSON.stringify({ "guesses": [], "solution": solutionId }))
 
         lista = localStorage.getItem(what)
     }
@@ -19,28 +19,44 @@ let initState = function (what, solutionId) {
     return solucion
 }
 
-function successRate (e){
-    // YOUR CODE HERE
+function successRate(e) {
+    return ((e.totalGames - e.gamesFailed) / e.totalGames) * 100
 }
 
-let getStats = function(what) {
+let getStats = function (what) {
     let lista = localStorage.getItem(what)
-    if(lista == null){
-        lista = JSON.stringify({winDistribution: [0,0,0,0,0,0,0,0,0],
+    if (lista == null) {
+        lista = JSON.stringify({
+            winDistribution: [0, 0, 0, 0, 0, 0, 0, 0, 0],
             gamesFailed: 0,
             currentStreak: 0,
             bestStreak: 0,
             totalGames: 0,
             successRate: 0
-            })
+        })
         localStorage.setItem(what, lista)
     }
     return lista
 };
 
 
-function updateStats(t){
+function updateStats(t) {
     let gameStats = JSON.parse(localStorage.getItem('gameStats'))
+
+    gameStats.winDistribution[t - 1]++
+    gameStats.totalGames++
+    if (t >= 8) {
+        gameStats.gamesFailed++
+        gameStats.currentStreak = 0
+    } else {
+        gameStats.currentStreak++
+        if (gameStats.currentStreak > gameStats.bestStreak) {
+            gameStats.bestStreak = gameStats.currentStreak
+        }
+    }
+    gameStats.successRate = successRate(gameStats)
+
+    localStorage.setItem("gameStats", JSOn.stringify(gameStats))
 
 };
 
