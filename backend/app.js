c9c3caa311fcfc9d3a64105a88659ff9c3de81a2
paabloomaartinez <pablo121002@gmail.com>
2022-12-17 13:37:55 +0100

    lo mismo que antes

--- backend/app.js
+++ backend/app.js
@@ -41,14 +41,7 @@ try{
         const url = `https://playfootball.games/who-are-ya/media/nations/${elem}.svg`
         fetch(url).then(res => {
             //check status
-            // 200: indica que todo ha ido correctamente.
-            // 302: indica que el recurso solicitado ha sido movido temporalmente a la URL dada por las cabeceras
-            // 401: indica que la petición (request) no ha sido ejecutada porque carece de credenciales válidas de autenticación para el recurso solicitado.
-            // 429: indica que el usuario ha enviado muchas peticiones (request) en un periodo de tiempo determinado ("limitación de velocidad")
-            // 500:  indica que el servidor encontró una condición inesperada que le impide completar la petición.
             if(res.status === 200) {
-                //res.body.pipe crea una "tuberia" (pipe) para relacionar el flujo de lectura con el fujo de escritura. ??ESTO MIRAR PORQUE NO ESTOY SEGURO
-                //fs.createWriteStream() crea un elemento en el path indicado en el cual se permite escribir. ??ESTO MIRAR PORQUE NO ESTOY SEGURO
                 res.body.pipe(fs.createWriteStream(`${writepath1}${elem}.svg`))
             }else {
                 console.log(`status: ${res.status} line: ${idx} elem: ${elem} not found`)
