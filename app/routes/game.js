f053f0689388e4a44b3bf29fb7171431629ee740
jonortega <jon.ortega.g@gmail.com>
2022-12-18 22:40:15 +0100

    Nuevas rutas creadas

--- app/routes/game.js
+++ app/routes/game.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('login');
+    // res.render('login');
+    res.send("Aqui iria el juego WhoAreYou")
 });
 
 module.exports = router;
