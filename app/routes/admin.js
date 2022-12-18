c9eacee3fc37c24f6767ccafbcee702dcd5545fa
paabloomaartinez <pablo121002@gmail.com>
2022-12-19 00:29:28 +0100

    estilos y form

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
-    res.render('form');
+    let element = { "id": "", "name": "", "birthdate": "", "nationality": "", "teamId": "", "position": "", "number": "", "leagueId": ""}
+    res.render('form', {element: element});
 });
 
 module.exports = router;
