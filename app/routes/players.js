3d3ce22884b825e5a34a0995593e1bef2ef40ab4
jonortega <jon.ortega.g@gmail.com>
2022-12-18 17:38:01 +0100

    Añadido las tareas

--- app/routes/players.js
+++ app/routes/players.js
@@ -4,51 +4,42 @@ const db = mongojs('mongodb://127.0.0.1:27017/footballdata', ['players'])
 var router = express.Router();
 
 
-// Visualizar datos del jugador
-router.get('/:id', function(req, res) {
-  db.footballdata.findOne({id: req.params.id }, (err, doc) => {
-    if (err) {
-        res.send(err);
-    } else {
-        console.log(doc)
-        res.render('player', { element: doc })
-    }
-})
-});
-
-
 // Función para borrar
-let remove = function(res, id){
-  db.footballdata.remove({_id: mongojs.ObjectId(id)}, (err, result) => {
-      if (err) {
-          res.send(err);
-      } else {
-          console.log(result);
-      }
+let remove = function (res, id) {
+  db.footballdata.remove({ _id: mongojs.ObjectId(id) }, (err, result) => {
+    if (err) {
+      res.send(err);
+    } else {
+      console.log(result);
+    }
   });
 }
 
+// Visualizar datos del jugador
+router.get('/:id', function (req, res) {
+  db.footballdata.findOne({ id: req.params.id }, (err, doc) => {
+    if (err) {
+      res.send(err);
+    } else {
+      console.log(doc)
+      res.render('player', { element: doc })
+    }
+  })
+});
+
 // Borrar datos del jugador
-router.get('/remove/:id', function(req, res) {
+router.get('/remove/:id', function (req, res) {
   remove(res, req.params.id);
 });
 
 // Crear jugador
-router.get('/add', (req, res) => {
-  res.render('add')
-})
-// Post
-router.post('/add', function(req, res) {
+router.post('/add', function (req, res) {
   //AÑADIR A LA BD EL JUGADOR
   res.send('added');
 });
 
 // Modificar datos del jugador
-router.get('/edit/:id', (req, res) => {
-  res.render('add')
-})
-// Put
-router.put('/edit/:id', function(req, res) {
+router.put('/edit/:id', function (req, res) {
   res.send('respond with a resource');
 });
 
