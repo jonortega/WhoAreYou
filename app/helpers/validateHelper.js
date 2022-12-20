7667ba51bb1febb9b2c5ff69520c5566ef2d0d8c
paabloomaartinez <pablo121002@gmail.com>
2022-12-20 15:04:53 +0100

    arreglando validator

--- app/helpers/validateHelper.js
+++ app/helpers/validateHelper.js
@@ -1,13 +1,18 @@
 const { validationResult } = require('express-validator')
 
 const validateResult = (req,res,next) => {
-    try{
-        validationResult(req).throw()
-        return next()
-      }catch(err){
-        res.status(400)
-        res.send("<h3>Existe un jugador con esa id</h3>")
-      }
+    // try{
+    //     validationResult(req)
+    //     return next()
+    // }catch(err){
+    //     res.status(400)
+    //     res.send({errors: err.array()})
+    // }
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).send({errors: errors.array()});
+    }
 }
 
 module.exports = { validateResult }
--- app/validators/add.js
+++ app/validators/add.js
@@ -1,9 +1,25 @@
 const { check } = require('express-validator')
 const { validateResult } = require('../helpers/validateHelper')
+const mongojs = require('mongojs')
+const db = mongojs('mongodb://127.0.0.1:27017/footballdata', ['players'])
 
 const validateCreate = [
-    check('id').isMongoId(), 
-    (req,res,next) => {
+    check('id').custom((value) => {
+        console.log(value)
+        db.players.find({ id: parseInt(value) }, (err, docs) => {
+            if (err) {
+              res.send(err);
+            } else {
+                if(docs[0]){
+                    console.log(docs[0])
+                    return Promise.reject('Id already taken')
+                }else {
+                    console.log(docs)
+                    return true
+                }
+            }
+        })
+    }), (req,res,next) => {
       validateResult(req,res,next)
     }
 ]
