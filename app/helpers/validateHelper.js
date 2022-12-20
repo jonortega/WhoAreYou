369cb89c65c32c02de517d894adfcf7dbd00899e
paabloomaartinez <pablo121002@gmail.com>
2022-12-20 23:57:07 +0100

    validator cambiado (no es vacio)

--- app/helpers/validateHelper.js
+++ app/helpers/validateHelper.js
@@ -1,18 +1,21 @@
 const { validationResult } = require('express-validator')
 
 const validateResult = (req,res,next) => {
-    // try{
-    //     validationResult(req)
-    //     return next()
-    // }catch(err){
-    //     res.status(400)
-    //     res.send({errors: err.array()})
-    // }
-
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).send({errors: errors.array()});
+    try{
+        validationResult(req).throw()
+        return next()
+    }catch(err){
+        res.status(400)
+        res.send('<h3>Rellena todos los campos.</h3>')
     }
+
+    // const errors = validationResult(req).throw();
+    // console.log(errors)
+    // if (!errors.isEmpty()) {
+    //     return res.status(400).send({errors: errors.array()});
+    // }else{
+    //     next()
+    // }
 }
 
 module.exports = { validateResult }
--- app/validators/add.js
+++ app/validators/add.js
@@ -4,22 +4,31 @@ const mongojs = require('mongojs')
 const db = mongojs('mongodb://127.0.0.1:27017/footballdata', ['players'])
 
 const validateCreate = [
-    check('id').custom((value) => {
-        console.log(value)
-        db.players.find({ id: parseInt(value) }, (err, docs) => {
-            if (err) {
-              res.send(err);
-            } else {
-                if(docs[0]){
-                    console.log(docs[0])
-                    return Promise.reject('Id already taken')
-                }else {
-                    console.log(docs)
-                    return true
-                }
-            }
-        })
-    }), (req,res,next) => {
+    check('id').not().isEmpty()
+        // .custom((value, {req}) => {
+        //     console.log(value)
+        //     db.players.find({ id: parseInt(value) }, (err, docs) => {
+        //         if (err) {
+        //             res.send(err);
+        //         } else {
+        //             if(docs[0]){
+        //                 console.log("Mal")
+        //                 throw new Error('Id already taken')
+        //             }
+        //             console.log("Bien")
+        //             req = {}
+        //             return true
+        //         }
+        //     })
+        // })
+    ,check('name').not().isEmpty(),
+    check('birthdate').not().isEmpty(),
+    check('nationality').not().isEmpty(),
+    check('teamId').not().isEmpty(),
+    check('position').not().isEmpty(),
+    check('number').not().isEmpty(),
+    check('leagueId').not().isEmpty()
+    , (req,res,next) => {
       validateResult(req,res,next)
     }
 ]
